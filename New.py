8cc7eb621e17985e913b0a4f1aac09d7a9ed5325
ms042087 <ms042087@gmail.com>
2018-02-14 18:52:03 +0800

    Update New.py

--- New.py
+++ New.py
@@ -4,25 +4,33 @@
 
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="ms084092", db="test")
+db = MySQLdb.connect(host="localhost", user="root", passwd="ms084092", db="sensorData")
 
 #create a cursor for the select
 cur = db.cursor()
 
 #execute an sql query
-cur.execute("SELECT firstname,lastname FROM test.name")
+cur.execute("SELECT DateTime, Temperature, Humidity FROM sensorData.TempHumid")
+
+print ("%s\t\t%s\t%s\t" %("DateTime","Temperature","Humidity"))
 
 ##Iterate 
 for row in cur.fetchall() :
       #data from rows
-      firstname = str(row[0])
-      lastname = str(row[1])
+      dateTime = str(row[0])
+      temperature = str(row[1])
+      humidity = str(row[2])
 
-      #print 
-      print ("This Person's name is " + firstname + " " + lastname)
+      #print
+      print ("%s\t%s\t\t%s\t" %(dateTime,temperature,humidity))
 
 # close the cursor
 cur.close()
 
 # close the connection
 db.close ()
+DateTime		Temperature	Humidity	
+2018-02-14 14:18:29	18		19	
+2018-02-14 14:18:45	18		19	
+2018-02-14 14:18:50	21		31	
+2018-02-14 14:18:55	18		19	
